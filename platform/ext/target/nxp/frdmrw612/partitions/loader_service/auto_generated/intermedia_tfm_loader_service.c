1223d5c39ea790225743df19e3dbf696e2032a9c
Waqar Tahir <waqar.tahir@nxp.com>
2024-07-02 15:23:20 +0200

    [nxp noup] added frdmrw612 board support in downstream fork of tfm v2.1 branch of nxp.
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

---
+++ platform/ext/target/nxp/frdmrw612/partitions/loader_service/auto_generated/intermedia_tfm_loader_service.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020-2022, Arm Limited. All rights reserved.
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+/***********  WARNING: This is an auto-generated file. Do not edit!  ***********/
+
+#include <stdint.h>
+#include "tfm_lds_config.h"
+
+uint8_t tfm_sp_lds_stack[LDS_STACK_SIZE] __attribute__((aligned(8)));
