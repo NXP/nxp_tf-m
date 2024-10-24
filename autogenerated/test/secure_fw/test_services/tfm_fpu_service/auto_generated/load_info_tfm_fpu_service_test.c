11bbf706e8dc034877e56238992a2d10bfe012bf
Ruchika Gupta <ruchika.gupta_1@nxp.com>
2024-10-24 15:57:51 +0530

    tf-m: Add auto-genrated files
    The auto-generated files for tf-m have already been copied to required
    destination.
    
    For tf-m tests compilation, the build system picks up the files from
    this folder.
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta_1@nxp.com>

---
+++ autogenerated/test/secure_fw/test_services/tfm_fpu_service/auto_generated/load_info_tfm_fpu_service_test.c
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2021-2022, Arm Limited. All rights reserved.
+ * Copyright (c) 2021-2023 Cypress Semiconductor Corporation (an Infineon
+ * company) or an affiliate of Cypress Semiconductor Corporation. All rights
+ * reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+/***********  WARNING: This is an auto-generated file. Do not edit!  ***********/
+
+#include <stdint.h>
+#include <stddef.h>
+#include "config_tfm.h"
+#include "region.h"
+#include "region_defs.h"
+#include "spm.h"
+#include "load/interrupt_defs.h"
+#include "load/partition_defs.h"
+#include "load/service_defs.h"
+#include "load/asset_defs.h"
+#include "tfm_peripherals_def.h"
+#include "psa_manifest/pid.h"
+#include "psa_manifest/sid.h"
+#include "psa_manifest/tfm_fpu_service_test.h"
+
+#define TFM_SP_FPU_SERVICE_TEST_NDEPS                           (0)
+#define TFM_SP_FPU_SERVICE_TEST_NSERVS                          (2)
+#if TFM_LVL == 3
+#define TFM_SP_FPU_SERVICE_TEST_NASSETS                         (0 + 1)
+#else
+#define TFM_SP_FPU_SERVICE_TEST_NASSETS                         (0)
+#endif
+#define TFM_SP_FPU_SERVICE_TEST_NIRQS                           (0)
+
+/* Memory region declaration */
+#if TFM_LVL == 3
+REGION_DECLARE(Image$$, PT_TFM_SP_FPU_SERVICE_TEST_PRIVATE, _DATA_START$$Base);
+REGION_DECLARE(Image$$, PT_TFM_SP_FPU_SERVICE_TEST_PRIVATE, _DATA_END$$Base);
+#endif
+
+extern uint8_t tfm_sp_fpu_service_test_stack[];
+
+/* Entrypoint function declaration */
+extern void fpu_service_test_main(void);
+
+/* Interrupt init functions */
+
+/* partition load info type definition */
+struct partition_tfm_sp_fpu_service_test_load_info_t {
+    /* common length load data */
+    struct partition_load_info_t    load_info;
+    /* per-partition variable length load data */
+    uintptr_t                       stack_addr;
+    uintptr_t                       heap_addr;
+    struct service_load_info_t      services[TFM_SP_FPU_SERVICE_TEST_NSERVS];
+#if TFM_LVL == 3
+    struct asset_desc_t             assets[TFM_SP_FPU_SERVICE_TEST_NASSETS];
+#else
+#endif
+} __attribute__((aligned(4)));
+
+/* Partition load, deps, service load data. Put to a dedicated section. */
+#if defined(__ICCARM__)
+#pragma location = ".part_load_priority_normal"
+__root
+#endif /* __ICCARM__ */
+const struct partition_tfm_sp_fpu_service_test_load_info_t tfm_sp_fpu_service_test_load
+    __attribute__((used, section(".part_load_priority_normal"))) = {
+    .load_info = {
+        .psa_ff_ver                 = 0x0101 | PARTITION_INFO_MAGIC,
+        .pid                        = TFM_SP_FPU_SERVICE_TEST,
+        .flags                      = 0
+                                    | PARTITION_MODEL_IPC
+                                    | PARTITION_PRI_NORMAL,
+        .entry                      = ENTRY_TO_POSITION(fpu_service_test_main),
+        .stack_size                 = 0x0400,
+        .heap_size                  = 0,
+        .ndeps                      = TFM_SP_FPU_SERVICE_TEST_NDEPS,
+        .nservices                  = TFM_SP_FPU_SERVICE_TEST_NSERVS,
+        .nassets                    = TFM_SP_FPU_SERVICE_TEST_NASSETS,
+        .nirqs                      = TFM_SP_FPU_SERVICE_TEST_NIRQS,
+    },
+    .stack_addr                     = (uintptr_t)tfm_sp_fpu_service_test_stack,
+    .heap_addr                      = 0,
+    .services = {
+        {
+            .name_strid             = STRING_PTR_TO_STRID("TFM_FPU_CHECK_FP_CALLEE_REGISTER"),
+            .sfn                    = 0,
+            .signal                 = TFM_FPU_CHECK_FP_CALLEE_REGISTER_SIGNAL,
+
+            .sid                    = 0x0000F091,
+            .flags                  = 0
+                                    | SERVICE_FLAG_NS_ACCESSIBLE
+                                    | SERVICE_VERSION_POLICY_STRICT,
+            .version                = 1,
+        },
+        {
+            .name_strid             = STRING_PTR_TO_STRID("TFM_FPU_TEST_NS_PREEMPT_S"),
+            .sfn                    = 0,
+            .signal                 = TFM_FPU_TEST_NS_PREEMPT_S_SIGNAL,
+
+            .sid                    = 0x0000F092,
+            .flags                  = 0
+                                    | SERVICE_FLAG_NS_ACCESSIBLE
+                                    | SERVICE_VERSION_POLICY_STRICT,
+            .version                = 1,
+        },
+    },
+#if TFM_LVL == 3
+    .assets                         = {
+        {
+            .mem.start              = (uintptr_t)&REGION_NAME(Image$$, PT_TFM_SP_FPU_SERVICE_TEST_PRIVATE, _DATA_START$$Base),
+            .mem.limit              = (uintptr_t)&REGION_NAME(Image$$, PT_TFM_SP_FPU_SERVICE_TEST_PRIVATE, _DATA_END$$Base),
+            .attr                   = ASSET_ATTR_READ_WRITE,
+        },
+    },
+#else
+#endif
+};
+
+/* Placeholder for partition and service runtime space. Do not reference it. */
+#if defined(__ICCARM__)
+#pragma location=".bss.part_runtime_priority_normal"
+__root
+#endif /* __ICCARM__ */
+static struct partition_t tfm_sp_fpu_service_test_partition_runtime_item
+    __attribute__((used, section(".bss.part_runtime_priority_normal")));
+#if defined(__ICCARM__)
+#pragma location = ".bss.serv_runtime_priority_normal"
+__root
+#endif /* __ICCARM__ */
+static struct service_t tfm_sp_fpu_service_test_service_runtime_item[TFM_SP_FPU_SERVICE_TEST_NSERVS]
+    __attribute__((used, section(".bss.serv_runtime_priority_normal")));
---
+++ autogenerated/test/secure_fw/test_services/tfm_fpu_service/psa_manifest/tfm_fpu_service_test.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2019-2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+/***********  WARNING: This is an auto-generated file. Do not edit!  ***********/
+
+#ifndef __PSA_MANIFEST_TFM_FPU_SERVICE_TEST_H__
+#define __PSA_MANIFEST_TFM_FPU_SERVICE_TEST_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TFM_SP_FPU_SERVICE_TEST_MODEL_IPC                       1
+#define TFM_SP_FPU_SERVICE_TEST_MODEL_SFN                       0
+
+#define TFM_FPU_CHECK_FP_CALLEE_REGISTER_SIGNAL                 (0x10)
+#define TFM_FPU_TEST_NS_PREEMPT_S_SIGNAL                        (0x20)
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __PSA_MANIFEST_TFM_FPU_SERVICE_TEST_H__ */
