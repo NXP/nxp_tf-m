ca3bed63c63ae5c570b3b2f51b7e62ded86550d2
Waqar Tahir <waqar.tahir@nxp.com>
2024-11-06 15:38:26 +0100

    [WCBRD-2011] Adding mcu-sdk cmake support for tfm for NXP platforms
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

---
+++ platform/ext/target/nxp/mcxn5xxevk/mcux.cmake
@@ -0,0 +1,63 @@
+# Copyright 2024 NXP
+# SPDX-License-Identifier: BSD-3-Clause
+
+if (CONFIG_MCUX_COMPONENT_middleware.tfm.s.board)
+	
+    mcux_add_macro(
+        CC "-DTARGET_CONFIG_HEADER_FILE=\\\"config_tfm_target.h\\\"\
+        "
+    )
+    mcux_add_source(
+        SOURCES 
+        ./common/target_cfg_common.h 
+        ./mcxn5xxevk/config_tfm_target.h 
+        ./mcxn5xxevk/target_cfg.c 
+        ./mcxn5xxevk/target_cfg.h 
+        ./mcxn5xxevk/tfm_peripherals_def.h 
+        ./mcxn5xxevk/Device/Include/platform_base_address.h 
+        ./mcxn5xxevk/partition/flash_layout.h 
+        ./mcxn5xxevk/partition/region_defs.h
+        ./mcxn5xxevk/Device/Source/startup_${board}.c
+        BASE_PATH ${SdkRootDirPath}/middleware/tfm/tf-m/platform/ext/target/nxp
+		BOARDS mcxn5xxevk
+    )
+    mcux_add_include(
+        INCLUDES 
+        ./common
+		./mcxn5xxevk 
+        ./mcxn5xxevk/Device/Include 
+        ./mcxn5xxevk/partition
+        BASE_PATH ${SdkRootDirPath}/middleware/tfm/tf-m/platform/ext/target/nxp
+		BOARDS mcxn5xxevk
+    )
+endif()
+
+if (CONFIG_MCUX_COMPONENT_middleware.tfm.ns.board)
+
+    mcux_add_macro(
+         CC "-DTARGET_CONFIG_HEADER_FILE=\\\"config_tfm_target.h\\\"\
+            "
+    )
+    mcux_add_source(
+        SOURCES 
+        ./common/target_cfg_common.h 
+        ./mcxn5xxevk/config_tfm_target.h 
+        ./mcxn5xxevk/target_cfg.h 
+        ./mcxn5xxevk/tfm_peripherals_def.h 
+        ./mcxn5xxevk/Device/Include/platform_base_address.h 
+        ./mcxn5xxevk/partition/region_defs.h 
+        ./mcxn5xxevk/partition/flash_layout.h
+        ./mcxn5xxevk/Device/Source/startup_${board}.c
+        BASE_PATH ${SdkRootDirPath}/middleware/tfm/tf-m/platform/ext/target/nxp
+		BOARDS mcxn5xxevk
+    )
+    mcux_add_include(
+        INCLUDES 
+        ./common 
+        ./mcxn5xxevk 
+        ./mcxn5xxevk/partition 
+        ./mcxn5xxevk/Device/Include
+        BASE_PATH ${SdkRootDirPath}/middleware/tfm/tf-m/platform/ext/target/nxp
+		BOARDS mcxn5xxevk
+    )
+endif()
