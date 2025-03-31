3e8089a44f446cd028388207911f5438182a0935
Waqar Tahir <waqar.tahir@nxp.com>
2025-03-31 10:50:27 +0200

    [IOTDL-2039] Quick build fix, for el2go_apps using mbedtls v3.6.3 as tfm/interface headers are being used in el2go examples _ns parts.
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

--- interface/include/psa/tfm/crypto_extra.h
+++ interface/include/psa/tfm/crypto_extra.h
@@ -112,8 +112,18 @@ extern "C" {
 #define PSA_ALG_IS_VENDOR_HASH_AND_SIGN(alg)    \
     PSA_ALG_IS_DSA(alg)
 
-/**@}*/
 
+/* If the size of static key slots is not explicitly defined by the user, then
+ * set it to the maximum between PSA_EXPORT_KEY_PAIR_OR_PUBLIC_MAX_SIZE and
+ * PSA_CIPHER_MAX_KEY_LENGTH.
+ * See mbedtls_config.h for the definition. */
+#if !defined(MBEDTLS_PSA_STATIC_KEY_SLOT_BUFFER_SIZE)
+#define MBEDTLS_PSA_STATIC_KEY_SLOT_BUFFER_SIZE  \
+    ((PSA_EXPORT_KEY_PAIR_OR_PUBLIC_MAX_SIZE > PSA_CIPHER_MAX_KEY_LENGTH) ? \
+     PSA_EXPORT_KEY_PAIR_OR_PUBLIC_MAX_SIZE : PSA_CIPHER_MAX_KEY_LENGTH)
+#endif /* !MBEDTLS_PSA_STATIC_KEY_SLOT_BUFFER_SIZE*/
+
+/**@}*/
 #ifdef __cplusplus
 }
 #endif
