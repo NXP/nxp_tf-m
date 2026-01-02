3d0b78cd1524ae77b84e349eaeb0de7c79139a94
Waqar Tahir <waqar.tahir@nxp.com>
2026-01-02 14:51:27 +0100

    [IOTDL-2487] Added mbedtls_accelerator_config.h file, which refers to NXP mcux_mbedtls_accelerator_config.h provided by mbedtls3x/port. With this change, no update required in default tfm provided mbedtls_config files.
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

---
+++ platform/ext/target/nxp/common/mbedtls_accelerator_config.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019-2022, Arm Limited. All rights reserved.
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef MBEDTLS_ACCELERATOR_CONFIG_H
+#define MBEDTLS_ACCELERATOR_CONFIG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#ifdef CRYPTO_HW_ACCELERATOR
+#include "mcux_mbedtls_accelerator_config.h"
+#endif
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* MBEDTLS_ACCELERATOR_CONFIG_H */
