486bbfac373dea5124f1d649667aef843104958e
waqar tahir <waqar.tahir@nxp.com>
2026-03-26 15:03:06 +0100

    [IOTDL-2713] Write in multiple of 4 pages when IPED is enabled to avoid keeping IPWR_EN enabled across multiple writes. Also, FLASH0_IPED_SECTOR_SIZE_LOG is re-calculated to always have number of pages multiple of 4.
    Signed-off-by: waqar tahir <waqar.tahir@nxp.com>

--- platform/ext/target/nxp/common/CMSIS_Driver/Driver_Flash_iap_rw61x_iped.c
+++ platform/ext/target/nxp/common/CMSIS_Driver/Driver_Flash_iap_rw61x_iped.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2013-2018 ARM Limited. All rights reserved.
- * Copyright 2019-2023, 2025 NXP
+ * Copyright 2019-2023, 2025-2026 NXP
  *
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -231,7 +231,7 @@ void printf_buffer(const char *name, const unsigned char *buffer, size_t size)
             *dst++ = nibble_to_char[(*src++) & 0x0f];
         }
         *dst = 0x00;
-        tfm_sp_log_printf("%s (0x%p): %s\r\n", name, src, chunk);
+        tfm_log_printf("%s (0x%p): %s\r\n", name, src, chunk);
         remaining -= block_size;
     }
 }
@@ -1229,21 +1229,27 @@ static int32_t ARM_Flash_ProgramData(uint32_t addr, const void *data, uint32_t c
 
         uint32_t dst_address     = sector_start_addr_phy;
         uint32_t remaining_pages = pages_per_sector;
-        while (remaining_pages > 0)
+
+        /* Switch execution to RAM/ROM and program Flash in blocks of 
+           4 pages (FLASH0_IPED_ALIGNED_PAGE_COUNT). This ensures IPWR_EN is 
+           enabled only once before the write sequence, and after the 4-page 
+           write completes (handled by ROM), IPWR_EN is disabled again before 
+           returning to Flash execution. */
+        while (remaining_pages >= FLASH0_IPED_ALIGNED_PAGE_COUNT)
         {
             TRACE_PAGE_WRITE("program page: ", dst_address);
-            TRACE_DATA("    programmed data", src_pos, page_size);
+            TRACE_DATA("programmed data", src_pos, page_size*FLASH0_IPED_ALIGNED_PAGE_COUNT);
 
             __disable_irq();
             clear_cache();
-            status_t status = iap_mem_write(&g_context, dst_address, page_size, src_pos, kMemoryID_FlexspiNor);
+            status_t status = iap_mem_write_blocked(&g_context, dst_address, page_size*FLASH0_IPED_ALIGNED_PAGE_COUNT, src_pos, kMemoryID_FlexspiNor);
             ASSERT_OR_EXIT(kStatus_Success == status, "iap_mem_write failed: ", status);
             clear_cache();
             __enable_irq();
 
-            dst_address += page_size;
-            src_pos += page_size;
-            remaining_pages -= 1;
+            dst_address += page_size*FLASH0_IPED_ALIGNED_PAGE_COUNT;
+            src_pos += page_size*FLASH0_IPED_ALIGNED_PAGE_COUNT;
+            remaining_pages -= FLASH0_IPED_ALIGNED_PAGE_COUNT;
         }
 
 #if FLASH0_IPED_METADATA_WRITE_ENABLED
