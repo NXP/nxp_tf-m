218a37ce9e0a4d62dba1cb9a7a5f9c1b63a9e7e3
waqar tahir <waqar.tahir@nxp.com>
2026-04-02 13:11:40 +0200

    [nxp noup] Platfrom: frdmrw612 updates
    cmake and config files updates for frdmrw612 board.
    Moreover, device, coresponding board and drivers files
    are updated as per sdk 26.03.00.
    
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

--- platform/ext/target/nxp/frdmrw612/project_template/bl2/board.c
+++ platform/ext/target/nxp/frdmrw612/project_template/bl2/board.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021-2025 NXP
+ * Copyright 2021-2024 NXP
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -15,8 +15,6 @@
 #include "fsl_power.h"
 #include "fsl_ocotp.h"
 #include "mcuxClEls.h"
-
-uint32_t __StackSeal = 0xDEADBEEF;
 
 /*******************************************************************************
  * Definitions
@@ -436,4 +434,17 @@ void BOARD_ClockPostConfig(void)
     else
     {
     }
+}
+
+/* RW61x is found some chips could not pull up some GPIO pins which are
+ * MUX with GAC ACOMP if GPIO power domain is 1.8V.
+ * The workaround is enable GAC MUX before using GPIOs (GPIO42-50, GPIO55 and GPIO58-61)
+ * input function
+ */
+void BOARD_ApplyGpioPullUpWorkaround(void)
+{
+    RSTCTL0->PRSTCTL1 &= ~RSTCTL0_PRSTCTL1_GAU_MASK; /* Remove reset GAU. */
+    CLKCTL0->PSCCTL1 |= CLKCTL0_PSCCTL1_GAU_MASK;    /* Enable GAU clock. */
+    GAU_ACOMP->CTRL0 |= ACOMP_CTRL0_EN_MASK;         /* Enable ACOMP0.    */
+    CLKCTL0->PSCCTL1 &= ~CLKCTL0_PSCCTL1_GAU_MASK;   /* Disable GAU clock. */
 }
